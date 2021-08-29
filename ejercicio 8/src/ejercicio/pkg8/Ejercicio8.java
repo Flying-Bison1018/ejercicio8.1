3be7b5f6919807fb524db9989a83f887924cb112
Flying-Bison1018 <danipina2003@gmail.com>
2021-08-28 19:28:11 -0500

    ejercicio 8.1

---
+++ ejercicio 8/src/ejercicio/pkg8/Ejercicio8.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg8;
+
+/**
+ *
+ * @author PC
+ */
+import java.util.Scanner;
+public class Ejercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println(
+                   "1. suma"
+                 + "2. restar"
+                 + "3.multiplicar"
+                 + "4.dividir"
+                 + "5 salir"
+        );
+        
+       
+        // TODO code application logic here
+    }
+    
+}
